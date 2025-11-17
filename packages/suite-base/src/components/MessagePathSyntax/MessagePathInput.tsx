e67302f12d89d5bf56025f3f3f55b688e003d34d
ctw-joao-luis <Joao.Luis@ctw.bmwgroup.com>
2025-11-17 09:25:05 +0000

    Panel toolbar visual bug fixes (#779)
    ## User-Facing Changes
    
    Fixed two visual bugs related to the panel Toolbar:
    
    - The Plot panel toolbar could occasionally overlap the panel content;
    - The clear icon in the Raw Messages autocomplete was misaligned and
    appeared too high relative to the input.
    
    ## Description
    
    Had to enforce certain CSS properties in order o fix both issues. The
    plot overlap bug was of unknown origin while the clear icon on Raw
    Messages was probably related to MUI version update.
    
    ## Checklist
    
    - [x] The web version was tested and it is running ok
    - [x] The desktop version was tested and it is running ok
    - [x] This change is covered by unit tests
    - [x] Files constants.ts, types.ts and *.style.ts have been checked and
    relevant code snippets have been relocated

--- packages/suite-base/src/components/MessagePathSyntax/MessagePathInput.tsx
+++ packages/suite-base/src/components/MessagePathSyntax/MessagePathInput.tsx
@@ -27,7 +27,8 @@ import {
   PrimitiveType,
 } from "@lichtblick/message-path";
 import * as PanelAPI from "@lichtblick/suite-base/PanelAPI";
-import { Autocomplete, IAutocomplete } from "@lichtblick/suite-base/components/Autocomplete";
+import { Autocomplete } from "@lichtblick/suite-base/components/Autocomplete";
+import { IAutocomplete } from "@lichtblick/suite-base/components/Autocomplete/types";
 import { useStructuredItemsByPath } from "@lichtblick/suite-base/components/MessagePathSyntax/useStructureItemsByPath";
 import useGlobalVariables, {
   GlobalVariables,
