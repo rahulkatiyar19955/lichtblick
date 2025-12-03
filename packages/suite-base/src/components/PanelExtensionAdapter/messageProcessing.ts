157e8fa67476fbc6ee53a215025dad8fd840c224
Rodrigo CTW <rodrigo.PE.rodrigues@ctw.bmwgroup.com>
2025-12-03 10:34:34 +0000

    Global Variables in MessageConverter (#799)
    ## User-Facing Changes
    
    For extension developers, global variables can now be accessed inside
    converter().
    
    ## Description
    
    This allows extensions to better cover their use cases.
    Some revisions to renderState had to be made which will, in the end,
    improve panel and extension functionality.
    Example extensions were developed to allow easier testing of this
    feature. Access `feature/test-global-variables` in
    https://github.com/lichtblick-suite/create-lichtblick-extension.
    
    Note: Only the "currentFrame" use case was changed. 
    
    Demo:
    Using e2e/fixtures/assets/example.mcap, get-global-variables converts
    MouseEvent into LocationFix.
    set-global-variables panel sets "lastMouseEvent" variable,
    get-global-variables panel uses this variable (when it's
    "aboveThreshold") to multiply the coordinates.
    
    
    https://github.com/user-attachments/assets/30addbb8-4e0d-4761-a60c-1bbb8dd20837
    
    
    https://github.com/user-attachments/assets/af004bf7-3df5-4c8f-80cd-735c732e43c7
    
    
    ## Checklist
    
    - [x] The web version was tested and it is running ok
    - [x] The desktop version was tested and it is running ok
    - [x] This change is covered by unit tests
    - [x] Files constants.ts, types.ts and *.style.ts have been checked and
    relevant code snippets have been relocated

--- packages/suite-base/src/components/PanelExtensionAdapter/messageProcessing.ts
+++ packages/suite-base/src/components/PanelExtensionAdapter/messageProcessing.ts
@@ -14,6 +14,7 @@ import {
   RegisterMessageConverterArgs,
   Subscription,
 } from "@lichtblick/suite";
+import { GlobalVariables } from "@lichtblick/suite-base/hooks/useGlobalVariables";
 import { Topic as PlayerTopic } from "@lichtblick/suite-base/players/types";
 import { Namespace } from "@lichtblick/suite-base/types";
 
@@ -42,11 +43,16 @@ export function convertMessage(
   messageEvent: Immutable<MessageEvent>,
   converters: Immutable<TopicSchemaConverterMap>,
   convertedMessages: MessageEvent[],
+  globalVariables?: Readonly<GlobalVariables>,
 ): void {
   const key = converterKey(messageEvent.topic, messageEvent.schemaName);
   const matchedConverters = converters.get(key);
   for (const converter of matchedConverters ?? []) {
-    const convertedMessage = converter.converter(messageEvent.message, messageEvent);
+    const convertedMessage = converter.converter(
+      messageEvent.message,
+      messageEvent,
+      globalVariables,
+    );
     // If the converter returns _undefined_ or _null_ the message is skipped
     if (convertedMessage == undefined) {
       continue;
